30dc5acd81dbd7b1feeb9370f5b55eb6eeacd741
dinhloc07 <86519485+dinhloc07@users.noreply.github.com>
2021-06-26 18:59:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ const CONFIG = {
     btnIntro: 'hahaha',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Em có thương anh hơm ',
-    btnYes: 'dạ có',
-    btnNo: 'Ứ, dỗi anh rồi',
+    btnYes: ' có',
+    btnNo: 'Ứ, giận anh rồi',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'hihi',
     reply: 'tại vì thích anh',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'đừng dỗi anh nữa nhá.',
-    btnAccept: 'dạ <3',
+    btnAccept: 'vâng <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
